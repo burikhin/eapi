2cffc46c86213ffc94230a0dc388192e02f382fc
Stanislav Burikhin <stanislav.burikhin@gmail.com>
2015-12-09 17:44:21 +0200

    update

--- users.js
+++ users.js
@@ -1,3 +1,79 @@
-/**
- * Created by stas on 09.12.15.
- */
+module.exports = [
+    {
+        "id": "1",
+        "fname": "Stanis",
+        "lname": "Baratheon",
+        "friends": [
+            "2",
+            "3"
+        ]
+    },
+    {
+        "id": "2",
+        "fname": "Stanis",
+        "lname": "Aclis",
+        "friends": [
+            "3"
+        ]
+    },
+    {
+        "id": "3",
+        "fname": "Robert",
+        "lname": "Baratheon",
+        "friends": [
+            "4"
+        ]
+    },
+    {
+        "id": "4",
+        "fname": "Kris",
+        "lname": "Kale",
+        "friends": [
+            "5"
+        ]
+    },
+    {
+        "id": "5",
+        "fname": "Nick",
+        "lname": "Green",
+        "friends": [
+            "6"
+        ]
+    },
+    {
+        "id": "6",
+        "fname": "Nino",
+        "lname": "Bogat",
+        "friends": [
+            "7",
+            "2"
+        ]
+    },
+    {
+        "id": "7",
+        "fname": "Tom",
+        "lname": "Red",
+        "friends": [
+            "7"
+        ]
+    },
+    {
+        "id": "13",
+        "fname": "S",
+        "lname": "B",
+        "friends": [],
+        "my_invites": [
+            "12"
+        ]
+    },
+    {
+        "id": "12",
+        "fname": "S",
+        "lname": "A",
+        "friends": [],
+        "my_invites": [],
+        "sent_invites": [
+            "13"
+        ]
+    }
+];
